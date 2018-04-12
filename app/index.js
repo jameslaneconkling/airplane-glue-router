b5dccda826c2d6d41733742944be5221eb52f025
James Conkling <james.lane.conkling@gmail.com>
2018-04-11 22:29:41 -0400

    make wikipedia-adapter location configurable

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ const {
 const createApp = require('./app');
 const makeMemoryRepository = require('./repositories/memory');
 const makeRemoteRepository = require('./repositories/remote');
-const PORT = process.env.PORT || 3000;
-const DEV_SEED = process.env.DEV_SEED || `${__dirname}/../seed.n3`;
 const {
   context
 } = require('./utils/rdf');
+
+const PORT = process.env.PORT || 3000;
+const DEV_SEED = process.env.DEV_SEED || `${__dirname}/../seed.n3`;
+const WIKIPEDIA_URL = process.env.WIKIPEDIA_URL || 'localhost:3001';
 
 
 const repos = [
@@ -20,12 +22,13 @@ const repos = [
   {
     name: 'wikipedia',
     domains: [/^dbr:/, /^dbo:/, /^dbp:/],
-    repository: makeRemoteRepository({ baseurl: 'http://localhost:3001/dbpedia', }),
+    repository: makeRemoteRepository({ baseurl: `http://${WIKIPEDIA_URL}/dbpedia`, }),
   }
 ];
 
 const app = createApp(repos, context);
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`)
+  console.log(`talking to wikipedia at ${WIKIPEDIA_URL}`);
 });
