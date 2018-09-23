0668242285e60484f3313923774850fd4e959fbf
James Conkling <james.lane.conkling@gmail.com>
2018-09-23 16:18:31 -0400

    remove unnecessary

--- src/falcor/index.ts
+++ src/falcor/index.ts
@@ -2,6 +2,7 @@ import FalcorServer from 'falcor-express';
 import Router from 'falcor-router';
 import { of } from 'rxjs';
 
+
 const BaseRouter = Router.createClass([
   {
     route: 'test',
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import falcor from './falcor';
 
@@ -12,8 +11,6 @@ const app = express();
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('combined'));
 }
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Falcor endpoint
 app.use('/api/model.json', falcor);
